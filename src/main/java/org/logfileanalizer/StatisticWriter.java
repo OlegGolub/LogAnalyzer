b7f4e36c626a4b9c28b4c6621873d2c2bf53ecbf
oleg.golubchik <oleg.golubchik@excelian.com>
2020-08-03 13:49:59 +0300

    Minor renames

--- src/main/java/org/logfileanalizer/StatisticWriter.java
+++ src/main/java/org/logfileanalizer/StatisticWriter.java
@@ -1,6 +1,6 @@
 package org.logfileanalizer;
 
 public interface StatisticWriter {
-    long writeStatistic(StatisticKeeper statisticProccessor, String fileName);
-    long writeStatistic(StatisticKeeper statisticProccessor);
+    long writeStatistic(StatisticKeeper statisticProcessor, String fileName);
+    long writeStatistic(StatisticKeeper statisticProcessor);
 }
